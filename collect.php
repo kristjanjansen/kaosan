b2175f2ab418271da2c39cd44688ee7062544d6b
kristjanjansen <kristjan.jansen@gmail.com>
2016-05-05 14:47:08 +0300

    fixes

--- collect.php
+++ collect.php
@@ -11,8 +11,9 @@ $pattern = "ad-a-*";
 $stat = [];
 
 foreach (new Iterator\Keyspace($redis, $pattern) as $key) {
+
     $el = explode('-', $key);
-    print_r($el);
+
     $stat[] = (object) [
         'id' => $el[2],
         $el[3] => $redis->get($key)
